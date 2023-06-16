37160086ed3b58bf469ecad1604b4a1ddcfcc8b3
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-17 01:59:56 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const products = [
-    {name: 'Laptopoo', price: 100000}, 
-    {name: 'Watch', price: 6000}, 
-    {name: 'Mobile', price: 31000},
-  ]
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, []);
   return (
     <div className="App">
-      {/* <Product name = 'Laptop' price = '100000'></Product>
-      <Product name = 'Watch' price = '6000'></Product>
-      <Product name = 'Mobile' price = '30000'></Product> */}
+      <h1>External Users</h1>
+      <p>{users.length}</p>
       {
-        products.map(product => <Product name = {product.name} price = {product.price}></Product>)
+        // users.map(user =><li>{user.name}</li>
+        users.map(user => <User name = {user.name} email = {user.email}></User>
+        )
       }
     </div>
   );
 }
-function Product(props) {
+function User(props) {
   return (
-    <div className='product'>
-      <h2>Name: {props.name}</h2>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>Name: {props.name}</h1>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+
+
 export default App;
+
+
+// const products = [
+//   {name: 'Laptopoo', price: 100000}, 
+//   {name: 'Watch', price: 6000}, 
+//   {name: 'Mobile', price: 31000},
+// ]
+
+
+// {/* <Product name = 'Laptop' price = '100000'></Product>
+//       <Product name = 'Watch' price = '6000'></Product>
+//       <Product name = 'Mobile' price = '30000'></Product> */}
+//       {
+//         products.map(product => <Product name = {product.name} price = {product.price}></Product>)
+//       }
+
+// function Product(props) {
+//   return (
+//     <div className='product'>
+//       <h2>Name: {props.name}</h2>
+//       <p>Price: {props.price}</p>
+//     </div>
+//   );
+// }
